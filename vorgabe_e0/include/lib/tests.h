dbb03d07b2f99fdbf41ec43c646c49ebba8e2b35
mah-diaa <mahmoud.diaa.abdeen@gmail.com>
2025-11-03 19:34:48 +0300

    Done (again)

--- vorgabe_e0/include/lib/tests.h
+++
@@ -1,7 +0,0 @@
-#ifndef KERNEL_TESTS_H
-#define KERNEL_TESTS_H
-
-void run_kprintf_tests(void);
-
-#endif /* KERNEL_TESTS_H */
-
--- vorgabe_e0/kernel/start.c
+++ vorgabe_e0/kernel/start.c
@@ -1,15 +1,11 @@
 #include <config.h>
 #include <arch/bsp/uart.h>
 #include <lib/kprintf.h>
-#include <lib/tests.h>
 
 void start_kernel()
 {
 	kprintf("=== Betriebssystem gestartet ===\n");
 	test_kernel();
-	
-	// Uncomment the line below to run kprintf tests
-	// run_kprintf_tests();
 
 	while (true) {
 		char c = uart_getc();
