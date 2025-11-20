a43a24984a1541b8795105e51defa6d8bfe26e8d
mah-diaa <mahmoud.diaa.abdeen@gmail.com>
2025-11-20 13:11:10 +0300

    refactor

--- vorgabe_e0/include/arch/cpu/exception_print.h
+++ vorgabe_e0/include/arch/cpu/exception_print.h
@@ -4,16 +4,13 @@
 #include "arch/cpu/registers.h"
 #include <stdbool.h>
 
-/* Print PSR (Program Status Register) flags and mode */
 void print_psr(unsigned int psr);
 
-/* Get mode name string from mode value */
 char *get_mode_name(unsigned int mode);
 
-/* Read mode-specific registers (SP, LR, CPSR/SPSR) for all modes */
 mode_regs_t read_mode_specific_registers(void);
 
-/* Print detailed exception information */
 void print_exception_infos(register_context_t* ctx, bool is_data_abort, bool is_prefetch_abort, char* exception_name, unsigned int exception_source_addr);
 
 #endif // EXCEPTION_PRINT_H_
+
--- vorgabe_e0/include/arch/cpu/exception_triggers.h
+++ vorgabe_e0/include/arch/cpu/exception_triggers.h
@@ -1,16 +1,13 @@
 #ifndef EXCEPTION_TRIGGERS_H_
 #define EXCEPTION_TRIGGERS_H_
 
-/* Trigger a Supervisor Call (SVC) exception */
 void do_supervisor_call(void);
 
-/* Trigger an Undefined Instruction exception */
 void do_undefined_inst(void);
 
-/* Trigger a Data Abort exception */
 void do_data_abort(void);
 
-/* Trigger a Prefetch Abort exception */
 void do_prefetch_abort(void);
 
 #endif // EXCEPTION_TRIGGERS_H_
+
